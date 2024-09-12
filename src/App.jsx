2b1751e56da661219b655b57460a708d419c02e0
JuliaYuki <julia.yuki.monte.ribeiro@gmail.com>
2024-09-11 23:19:00 -0300

    Integrando as Rotas no App.js
    Rota Protegida para Guias:
    A rota /dashboard-guide está protegida pelo componente PrivateRouteGuide, que verifica se o usuário é um Guia Turístico. Caso contrário, ele será redirecionado para o login.
    
    Dashboard para Guias:
    Se o usuário for um guia autenticado, ele poderá acessar a página Dashboard do Guia que exibe a quantidade de passeios cadastrados e atalhos para páginas de cadastro e listagem de passeios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,87 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import DashboardGuide from "./pages/DashboardGuidePage/DashboardGuide";
+import PrivateGuideRoute from "./components/PrivateGuideRoute/PrivateGuideRoute";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Logout from "./components/Logout/Logout";
 
-function App() {
+function Navbar() {
+  const session = JSON.parse(localStorage.getItem("session"));
+  const location = useLocation();
+
   return (
-    <>
-      <Router>
-        <nav>
-          {" "}
-          {/* Move the nav element outside of the div */}
-          <ul>
-            <li>
-              <Link className="link" to="/login">
-                Login
-              </Link>
-            </li>
+    <nav>
+      <ul>
+        {session && location.pathname !== "/register" && (
+          <li>
+            <Link className="link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+        )}
+
+        {!session && location.pathname !== "/login" && (
+          <li>
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          </li>
+        )}
+
+        {!session &&
+          location.pathname !== "/register" &&
+          location.pathname !== "/dashboard" && (
             <li>
               <Link className="link" to="/register">
                 Cadastro
               </Link>
             </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </Router>
-    </>
+          )}
+
+        {session && location.pathname === "/dashboard" && (
+          <li>
+            <Logout />
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/dashboard-guide"
+          element={
+            <PrivateGuideRoute>
+              <DashboardGuide />
+            </PrivateGuideRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <h2>Bem-vindo ao Dashboard!</h2>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
