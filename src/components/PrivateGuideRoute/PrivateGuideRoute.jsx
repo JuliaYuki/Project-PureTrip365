c06f0542df9216e2ee763669b23911235610838c
JuliaYuki <julia.yuki.monte.ribeiro@gmail.com>
2024-09-11 23:18:05 -0300

    Protegendo o Acesso à Rota /dashboard-guide
    Criando o Componente PrivateRouteGuide;
    
    Verificação do Perfil:
    O componente verifica se o usuário está autenticado e se o perfil do usuário é Guia Turístico.
    Se o usuário não estiver logado ou não tiver o perfil de guia, ele será redirecionado para a página de login (/login).

---
+++ src/components/PrivateGuideRoute/PrivateGuideRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+export default function PrivateGuideRoute({ children }) {
+  const session = JSON.parse(localStorage.getItem("session"));
+
+  if (!session || session.role !== "Guia Turístico") {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
