521624b8c824df3f2e3727de08a0941ef86664f6
JuliaYuki <julia.yuki.monte.ribeiro@gmail.com>
2024-09-20 20:47:37 -0300

    assets e dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,26 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  useLocation,
-} from "react-router-dom";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import DashboardGuide from "./pages/DashboardGuidePage/DashboardGuide";
-import PrivateGuideRoute from "./components/PrivateGuideRoute/PrivateGuideRoute";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import Logout from "./components/Logout/Logout";
-
-function Navbar() {
-  const session = JSON.parse(localStorage.getItem("session"));
-  const location = useLocation();
-
-  return (
-    <nav>
-      <ul>
-        {session && location.pathname !== "/register" && (
-          <li>
-            <Link className="link" to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-        )}
-
-        {!session && location.pathname !== "/login" && (
-          <li>
-            <Link className="link" to="/login">
-              Login
-            </Link>
-          </li>
-        )}
-
-        {!session &&
-          location.pathname !== "/register" &&
-          location.pathname !== "/dashboard" && (
-            <li>
-              <Link className="link" to="/register">
-                Cadastro
-              </Link>
-            </li>
-          )}
-
-        {session &&
-          (location.pathname === "/dashboard" ||
-            location.pathname === "/dashboard-guide") && (
-            <li>
-              <Logout />
-            </li>
-          )}
-      </ul>
-    </nav>
-  );
-}
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage/LoginPage'
+import RegisterPage from './pages/RegisterPage/RegisterPage'
+import DashboardGuide from './pages/DashboardGuidePage/DashboardGuide'
+import PrivateGuideRoute from './components/PrivateGuideRoute/PrivateGuideRoute'
+import PrivateRoute from './components/PrivateRoute/PrivateRoute'
+import NewTour from './pages/NewTourPage/NewTour'
+import TourList from './pages/TourListPage/TourList'
+import TourDetail from './pages/TourDetailPage/TourDetail'
+import Reservations from './pages/ReservationsPage/Reservations'
+import PrivateRouteTourist from './components/PrivateRouteTourist/PrivateRouteTourist'
+import Navbar from './components/NavBar/NavBar'
+import TourReviews from './pages/TourReviewsPage/TourReviews'
+import TouristProfile from './pages/TouristProfilePage/TouristProfile'
+import EditTour from './pages/EditTourPage/EditTour'
+import ReviewsPage from './pages/TourReviewsPage/ReviewsPage'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -74,17 +33,74 @@ function App() {
             </PrivateGuideRoute>
           }
         />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route
-          path="/dashboard"
+          path="/passeio/novo"
+          element={
+            <PrivateGuideRoute>
+              <NewTour />
+            </PrivateGuideRoute>
+          }
+        />
+        <Route
+          path="/passeios"
           element={
             <PrivateRoute>
-              <h2>Bem-vindo ao Dashboard!</h2>
+              <TourList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/passeios/:id"
+          element={
+            <PrivateRoute>
+              <TourDetail />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/passeio/editar/:id"
+          element={
+            <PrivateGuideRoute>
+              <EditTour />
+            </PrivateGuideRoute>
+          }
+        />
+        <Route
+          path="/reservas"
+          element={
+            <PrivateRoute>
+              <Reservations />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/avaliacoes/:id"
+          element={
+            <PrivateRoute>
+              <TourReviews />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/perfil-turista"
+          element={
+            <PrivateRoute>
+              <TouristProfile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/avaliacoes"
+          element={
+            <PrivateRoute>
+              <ReviewsPage />
             </PrivateRoute>
           }
         />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
